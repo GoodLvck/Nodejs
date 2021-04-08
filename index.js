546773d34e165142cc0e1c3e1ea10d5623abeadc
GoodLvck <x17albaserrano17x@gmail.com>
2021-04-08 13:22:29 +0200

    changes

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+/*
 const http = require('http');
 const colors =  require('colors');
 
@@ -8,6 +9,20 @@ const handleServer = function(req, res){
 }
 
 const server = http.createServer(handleServer);
+
+server.listen(3000, function(){
+    console.log('Server on port 3000'.green);
+});
+*/
+
+const express = require('express');
+const colors = require('colors');
+
+const server = express();
+
+server.get('/', function(req, res){
+    res.send('<h1>Hola Mundo desde Nodejs y Express</h1>');
+});
 
 server.listen(3000, function(){
     console.log('Server on port 3000'.green);
